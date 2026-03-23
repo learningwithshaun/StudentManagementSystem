cc7bb4c9b3fab9fd0d4678e1894c9296fa351ded
samthepessimist <231116144@mycput.ac.za>
2026-03-24 00:55:21 +0200

    added print statement in my factory class

---
+++ src/main/java/za/ac/cput/factory/Factory.java
@@ -0,0 +1,7 @@
+package za.ac.cput.factory;
+
+public class Factory {
+    public static void main(String[] args) {
+        System.out.println("This is my factory class where my factory method will be");
+    }
+}
